a7859b2ec9e712d44143a83c393ab920f0175d46
Rithik Pahariya <79541360+rithikpahariya@users.noreply.github.com>
2025-11-19 08:05:30 +0000

    Merge branches 'draft/white-night' and 'master' of https://github.com/rithikpahariya/My-portfolio

--- components/contributions/contribution-card.tsx
+++ components/contributions/contribution-card.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import { Icons } from "@/components/common/icons";
 import { contributionsInterface } from "@/config/contributions";
 
@@ -13,19 +11,19 @@ export default function ContributionCard({
   return (
     <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3 static">
       {contributions.map((contribution, id) => (
-        <Link href={contribution.link} target="_blank" key={id}>
+        <div key={id}>
           <div className="relative rounded-lg border bg-background p-2 hover:bg-accent hover:text-accent-foreground">
-            <Icons.externalLink
+            {/* <Icons.externalLink
               size={35}
               className="absolute bottom-3 right-3 border bg-background rounded-full p-2 cursor-pointer text-muted-foreground "
-            />
+            /> */}
             <div className="flex h-[170px] flex-col justify-between rounded-md p-6 sm:h-[170px]">
               <div className="flex flex-row justify-between">
                 <h3 className="font-bold flex space-x-2 items-center">
                   <Icons.gitRepoIcon size={20} />
                   <span>{contribution.repo}</span>
                 </h3>
-                <Icons.gitBranch size={20} />
+                {/* <Icons.gitBranch size={20} /> */}
               </div>
               <div className="space-y-4">
                 <p className="text-sm text-muted-foreground">
@@ -38,7 +36,7 @@ export default function ContributionCard({
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
